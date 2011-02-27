4041adb4ef233168f9f89347eda799f1795e1ed5
liebharc <liebharc@37091a83-eecf-406c-9c4e-d5ec8d4f716c>
2011-02-27 13:22:43 +0000

    Cleaned dependencies and removed proguard.

--- project/build/MmTasksPdfPrinter.scala
+++ project/build/MmTasksPdfPrinter.scala
@@ -6,12 +6,9 @@ class MmTasksPdfPrinter(info: ProjectInfo) extends DefaultWebProject(info) with 
 
   val suffix = ".war"
   override lazy val jarPath: Path = outputPath / (defaultJarBaseName + suffix)
-  val shrinkedJarName = defaultJarBaseName + "Shrinked" + suffix
-  lazy val shrinkedJarPath: Path = outputPath / shrinkedJarName
-	val proguardConfigurationPath: Path = outputPath / "proguard.pro"
+  val jarName = defaultJarBaseName + suffix
 
   val toolsConfig = config("tools")
-  val proguardLibsConfig = config("proguard")
 
   val iTextUrl = new java.net.URL("http://maven.itextpdf.com/")
   val iTextRepo = Resolver.url("com.itextpdf", iTextUrl)
@@ -19,106 +16,17 @@ class MmTasksPdfPrinter(info: ProjectInfo) extends DefaultWebProject(info) with 
   val lift = "net.liftweb" %% "lift-mapper" % "2.1" % "compile->default"
   val commons = "commons-lang" % "commons-lang" % "2.4" % "compile->default"
   val itext = "com.itextpdf" % "itextpdf" % "5.0.2" % "compile->default"
-  val servlet = "javax.servlet" % "servlet-api" % "2.3" % "proguard->default"
-  /*
-   * xmlrpcClient dependencies needed to be resolved manually since:
-   * xmlrpcClient depends on xmLrpcCommon which depends on wsCommons which depends on xml-apis
-   * xml-apis may not be added as dependency since it brings jce.jar would then
-   * also depend on xml-apis and this would cause proguard to fail
-   */
-  val xmlrpcClient = "org.apache.xmlrpc" % "xmlrpc-client" % "3.1.3" % "compile->default" intransitive
-  val xmlrpcCommon = "org.apache.xmlrpc" % "xmlrpc-common" % "3.1.3" % "compile->default" intransitive
-  val wsCommons = "org.apache.ws.commons" % "ws-commons-util" % "1.0.1" % "compile->default" intransitive
-  val jiraSoap = "com.dolby.atlassian.jira" % "jira-soap" % "1.0-3.13.4" % "compile->default"
+  val xmlrpcClient = "org.apache.xmlrpc" % "xmlrpc-client" % "3.1.3" % "compile->default"
   val jerichoHtmlParser = "net.htmlparser.jericho" % "jericho-html" % "3.1" % "compile->default"
   val httpClient = "commons-httpclient" % "commons-httpclient" % "3.1" % "compile->default"
   val jettyDep =  "org.mortbay.jetty" % "jetty" % "6.1.22" % "test->default"
   val junit = "junit" % "junit" % "4.5" % "test->default"
   val scalatest = "org.scalatest" % "scalatest" % "1.2" % "test->default"
-  val proguardDep = "net.sf.proguard" % "proguard" % "4.4" % "tools->default"
   val container = "org.jvnet.hudson.winstone" % "winstone" % "0.9.10-hudson-24" % "tools->default"
 
-  override lazy val `package` = packageAction dependsOn(createClassesJar, removeClasses)
-  lazy val createClassesJar = createClassesJarTask(temporaryWarPath) dependsOn(prepareWebapp) describedAs ("Creating classes.jar")
-  lazy val removeClasses = removeClassesTask(temporaryWarPath) dependsOn(prepareWebapp, createClassesJar) describedAs ("Removes class files in WEB-INF/classes")
-  lazy val proguardConfig = writeProguardConfigurationTask describedAs("Creating proguard config")
   lazy val fetchContainer = fetchContainerTask dependsOn() describedAs("Adds a servlet container to target dir")
-  lazy val writeLauncherScript = writeLauncherScriptTask("startMmPrinter.sh", container.name+"-"+container.revision+".jar", shrinkedJarName)  dependsOn(fetchContainer) describedAs("Writes a small start script")
-  lazy val proguard = proguardTask dependsOn(`package`, proguardConfig, writeLauncherScript) describedAs("Reduces size of packaged war using ProGuard")
-
-  private def createClassesJarTask(warPath: => Path) = task {
-    val webInfPath = warPath / "WEB-INF"
-    val webLibDirectory = webInfPath / "lib"
-    val classesTargetDirectory = webInfPath / "classes"
-
-    FileUtilities.clean(webLibDirectory / "classes.jar" :: Nil, log)
-    FileUtilities.zip(descendents(classesTargetDirectory ##, "*.class").get, webLibDirectory / "classes.jar", false, log)
-  }
-
-  private def removeClassesTask(warPath: => Path) = task {
-    val webInfPath = warPath / "WEB-INF"
-    val classesTargetDirectory = webInfPath / "classes"
-    FileUtilities.clean(classesTargetDirectory / "com" :: classesTargetDirectory / "bootstrap":: Nil, log)
-    None 
-  }
-
-	private def writeProguardConfigurationTask = task {
-    val outTemplate = """
-      |-dontoptimize
-      |-dontobfuscate
-      |-injars %s
-      |-outjars %s
-      |-libraryjars <java.home>/lib/rt.jar 
-      |-libraryjars <java.home>/lib/jce.jar
-      |-libraryjars %s
-      |-dontskipnonpubliclibraryclasses
-      |-dontskipnonpubliclibraryclassmembers
-      |-dontnote
-      |-ignorewarnings
-      |-dontwarn **$$anonfun$*
-      |-dontwarn scala.collection.immutable.RedBlack$Empty
-      |-dontwarn scala.tools.**,plugintemplate.**
-      |
-      |-keep class * implements org.xml.sax.EntityResolver
-      |
-      |-keepclassmembers class * {
-      |    ** MODULE$;
-      |}
-      |-keep public class bootstrap.liftweb.Boot {
-      | public *;
-      |}
-      |-keep public class com.tngtech.** {
-      | public *;
-      |}
-      |-keep public class * extends javax.servlet.** {
-      | public *;
-      |}
-      |-keep public class * extends org.apache.log4j.Appender {
-      | public *;
-      |}
-      |-keep public class * extends org.apache.log4j.Layout {
-      | public *;
-      |}
-      |"""
-      val libClasspath = managedClasspath(proguardLibsConfig)
-      val libClasspathString = Path.makeString(libClasspath.get)
-      val proguardConfiguration =
-        outTemplate.stripMargin.format(
-          mkpath(jarPath.asFile),
-          mkpath(shrinkedJarPath.asFile),
-          libClasspathString)
-			FileUtilities.write(proguardConfigurationPath.asFile, proguardConfiguration, log)
-      None
-  }
-
-  private def proguardTask = task {
-    FileUtilities.clean(shrinkedJarPath :: Nil, log)
-    val proguardClasspath = managedClasspath(toolsConfig)
-    val proguardClasspathString = Path.makeString(proguardClasspath.get)
-    val configFile = proguardConfigurationPath.asFile.getAbsolutePath
-    val exitValue = Process("java", List("-Xmx512M", "-cp", proguardClasspathString, "proguard.ProGuard", "@" + configFile)) ! log 
-    if (exitValue == 0) None else Some("Proguard failed with nonzero exit code (" + exitValue + ")")
-  }
+  lazy val writeLauncherScript = writeLauncherScriptTask("startMmPrinter.sh", container.name+"-"+container.revision+".jar", jarName)  dependsOn(fetchContainer) describedAs("Writes a small start script")
+  override lazy val `package` = packageAction dependsOn(writeLauncherScript)
 
 private def writeLauncherScriptTask(scriptName: String, warName: String, containerName: String) = task {
   val scriptTemplate = """
