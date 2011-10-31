195c8b91a876552e95d925827590e1a1166d242c
himmelmr <himmelmr@37091a83-eecf-406c-9c4e-d5ec8d4f716c>
2011-10-31 10:28:19 +0000

    Small refactorings

--- src/main/scala/com/tngtech/mmtaskspdfprinter/creation/pdf/StoryPrinter.scala
+++ src/main/scala/com/tngtech/mmtaskspdfprinter/creation/pdf/StoryPrinter.scala
@@ -7,17 +7,17 @@ import com.itextpdf.text.pdf._
 import com.tngtech.mmtaskspdfprinter.scrum._
 import com.tngtech.mmtaskspdfprinter.creation.pdf.config._
 
-class StoryPrinter2(val contentSize: Rectangle, val config: PdfConfiguration) {
+class StoryPrinter(val contentSize: Rectangle, val config: PdfConfiguration) {
 
   import config.size.{ rowNumber => rowSize }
 
   def create(stories: Seq[Story]): Seq[PdfPTable] = {
     val (withoutAcceptance, withAcceptance) = stories span (_.acceptanceCriteria.isEmpty)
 
-    val l1 = withAcceptance flatMap { s => Vector(createStoryRow(s), createAcceptanceRow(s)) }
-    val l2 = withoutAcceptance map createStoryRow
+    val l1 = withAcceptance flatMap { s => Vector(createStoryCard(s), createAcceptanceCard(s)) }
+    val l2 = withoutAcceptance map createStoryCard
 
-    val emptyCell = createStoryRow(Story("", UndefScrumPoints, None, Seq()))
+    val emptyCell = createStoryCard(Story("", UndefScrumPoints, None, Seq()))
 
     val pages = (l1 ++ l2) grouped 2 map { cells =>
       val page = new PdfPTable(1)
@@ -33,13 +33,36 @@ class StoryPrinter2(val contentSize: Rectangle, val config: PdfConfiguration) {
   }
 
   val sepHeight = 5
-  val contentHeight = contentSize.getHeight() / rowSize -
-      createFooter().getFixedHeight - sepHeight * 2
 
-  def createAcceptanceRow(story: Story): PdfPCell = {
+  private val separator: PdfPCell = {
+    val cell = new PdfPCell(config.companyBanner, false)
+    cell.setBorder(Rectangle.NO_BORDER)
+    cell.setPadding(0)
+    cell.setIndent(0)
+    cell.setFixedHeight(sepHeight)
+    cell.setColspan(2)
+    cell.setBackgroundColor(new BaseColor(config.colour(0), config.colour(1), config.colour(2)))
+    cell
+  }
+
+  private val footer: PdfPCell = {
+    val cell = new PdfPCell(config.companyBanner, false)
+    cell.setBorder(Rectangle.NO_BORDER)
+    cell.setPadding(1)
+    cell.setPaddingLeft(5)
+    cell.setIndent(0)
+    cell.setFixedHeight(config.companyBanner.getScaledHeight +
+      cell.getPaddingTop + cell.getPaddingBottom)
+    cell.setColspan(2)
+    cell
+  }
+
+  val contentHeight = contentSize.getHeight() / rowSize -
+    footer.getFixedHeight - sepHeight * 2
+
+  def createAcceptanceCard(story: Story): PdfPCell = {
     val innerTable = new PdfPTable(1)
     innerTable.setWidthPercentage(100.0f)
-    innerTable.setWidths(Array(100))
 
     val list = new ITextList(ITextList.UNORDERED)
     for (s <- story.acceptanceCriteria)
@@ -58,63 +81,42 @@ class StoryPrinter2(val contentSize: Rectangle, val config: PdfConfiguration) {
       headerCell
     }
 
-    innerTable.addCell(createSeparator(sepHeight))
+    innerTable.addCell(separator)
     innerTable.addCell(headerCell)
-    innerTable.addCell(createFooter())
-    innerTable.addCell(createSeparator(sepHeight))
+    innerTable.addCell(footer)
+    innerTable.addCell(separator)
 
     new PdfPCell(innerTable)
   }
 
-  def createStoryRow(story: Story): PdfPCell = {
+  def createStoryCard(story: Story): PdfPCell = {
     val innerTable = new PdfPTable(2)
     innerTable.setWidthPercentage(100.0f)
     innerTable.setWidths(Array(70, 30))
-    innerTable.addCell(createSeparator(sepHeight))
-    innerTable.addCell(createStoryCell(contentHeight, story))
-    innerTable.addCell(createMetaCell(contentHeight, story))
-    innerTable.addCell(createFooter())
-    innerTable.addCell(createSeparator(sepHeight))
+
+    val storyCell = {
+      val storyPhrase = new Phrase()
+      storyPhrase.add(new Chunk("\n" + story.name, config.size.hugeFont))
+      storyPhrase.add(new Chunk("\n\n" + story.jiraKey, config.size.normalFont))
+      val storyCell = new PdfPCell(storyPhrase)
+      storyCell.setBorder(Rectangle.NO_BORDER)
+      storyCell.setPadding(5)
+      storyCell.setPaddingRight(30)
+      storyCell.setIndent(0)
+      storyCell.setFixedHeight(contentHeight)
+      storyCell
+    }
+
+    innerTable.addCell(separator)
+    innerTable.addCell(storyCell)
+    innerTable.addCell(createMetaCell(story))
+    innerTable.addCell(footer)
+    innerTable.addCell(separator)
+
     new PdfPCell(innerTable)
   }
 
-  private def createSeparator(height: Int): PdfPCell = {
-    val cell = new PdfPCell(config.companyBanner, false)
-    cell.setBorder(Rectangle.NO_BORDER)
-    cell.setPadding(0)
-    cell.setIndent(0)
-    cell.setFixedHeight(height)
-    cell.setColspan(2)
-    cell.setBackgroundColor(new BaseColor(config.colour(0), config.colour(1), config.colour(2)))
-    cell
-  }
-
-  private def createFooter(): PdfPCell = {
-    val cell = new PdfPCell(config.companyBanner, false)
-    cell.setBorder(Rectangle.NO_BORDER)
-    cell.setPadding(1)
-    cell.setPaddingLeft(5)
-    cell.setIndent(0)
-    cell.setFixedHeight(config.companyBanner.getScaledHeight +
-      cell.getPaddingTop + cell.getPaddingBottom)
-    cell.setColspan(2)
-    cell
-  }
-
-  private def createStoryCell(height: Float, story: Story): PdfPCell = {
-    val storyPhrase = new Phrase()
-    storyPhrase.add(new Chunk("\n" + story.name, config.size.hugeFont))
-    storyPhrase.add(new Chunk("\n\n" + story.jiraKey, config.size.normalFont))
-    val storyCell = new PdfPCell(storyPhrase)
-    storyCell.setBorder(Rectangle.NO_BORDER)
-    storyCell.setPadding(5)
-    storyCell.setPaddingRight(30)
-    storyCell.setIndent(0)
-    storyCell.setFixedHeight(height)
-    storyCell
-  }
-
-  private def createMetaCell(height: Float, story: Story): PdfPCell = {
+  private def createMetaCell(story: Story): PdfPCell = {
     val metaPhrase = new Phrase()
     val undefined = "________"
     val priority = story.priority.getOrElse(undefined).toString
