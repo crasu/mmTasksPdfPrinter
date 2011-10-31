db8bac5aa7f09431688ea83da72943f647abb8b2
himmelmr <himmelmr@37091a83-eecf-406c-9c4e-d5ec8d4f716c>
2011-10-31 11:54:24 +0000

    Added test for StoryPrinter

--- src/test/scala/com/tngtech/mmtaskspdfprinter/creation/pdf/StoryPrinterTest.scala
+++ src/test/scala/com/tngtech/mmtaskspdfprinter/creation/pdf/StoryPrinterTest.scala
@@ -1,17 +1,33 @@
 package com.tngtech.mmtaskspdfprinter.creation.pdf
 
 import org.specs2.mutable._
+import com.tngtech.mmtaskspdfprinter.creation.pdf.config.PdfConfiguration
+
+import com.tngtech.mmtaskspdfprinter.scrum._
+import com.itextpdf.text.{ List => ITextList, _ }
+import com.itextpdf.text.pdf._
 
 class StoryPrinterTest extends Specification {
   "The StoryPrinter" should {
-    "contain 11 characters" in {
-      "Hello world" must have size (11)
+    var storyCards = 0
+    var acceptanceCards = 0
+    val printer = new StoryPrinter(new Rectangle(100, 100),
+      PdfConfiguration.defaultConfig) {
+      override def createStoryCard(story: Story) =
+        { storyCards += 1; new PdfPCell() }
+      override def createAcceptanceCard(story: Story) =
+        { acceptanceCards += 1; new PdfPCell() }
     }
-    "start with 'Hello'" in {
-      "Hello world" must startWith("Hello")
+    val s = List(Story("", null, None, Nil),
+        Story("", null, None, Nil),
+        Story("", null, None, Nil, List("", "", "")))
+    printer.create(s)
+    "create the correct number of story cards" in {
+      // Add one for the empty card.
+      storyCards must beEqualTo(3 + 1)
     }
-    "end with 'world'" in {
-      "Hello world" must endWith("world")
+    "create the correct number of acceptance cards" in {
+      acceptanceCards must beEqualTo(1)
     }
   }
 }
