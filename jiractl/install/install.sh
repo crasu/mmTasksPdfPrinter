344d73d5936afc369db711175d6d1e0a87dc3988
schmidr <schmidr@37091a83-eecf-406c-9c4e-d5ec8d4f716c>
2012-03-13 14:39:13 +0000

    Minor Bugfix

--- jiractl/install/install.sh
+++ jiractl/install/install.sh
@@ -10,6 +10,9 @@ read decision
 if [ $decision = "n" ] || [ $decision = "N" ] ; then
   echo "Done."
 else
+  if [ ! -d /var/log/mongodb ] ; then
+    sudo mkdir -p /var/log/mongodb
+  fi
   sudo cp -t /etc/init mongodb.conf
 fi
 echo "Please cd to your jiractl-Directory and run 'npm install' (you might need to specify the Path to npm)"
