008bf8c9332377657e9093691d695c1f26c04637
eckertj <eckertj@37091a83-eecf-406c-9c4e-d5ec8d4f716c>
2012-09-07 13:44:27 +0000

    Removed tabs in this file

--- src/main/scala/com/tngtech/mmtaskspdfprinter/creation/jira/SoapClient.scala
+++ src/main/scala/com/tngtech/mmtaskspdfprinter/creation/jira/SoapClient.scala
@@ -22,18 +22,18 @@ object TrivialSoapClient extends SoapClient {
   def sendMessage(url: String, action: String, req: Elem) = {
     val postMethod = new PostMethod(url)
     try {
-	    postMethod.setRequestEntity(new StringRequestEntity(wrap(req), "text/xml", "utf-8"))
-	    postMethod.setRequestHeader("SOAPAction", action)    
-	    val client = new HttpClient()
-	    val status = client.executeMethod(postMethod)
-	    val responseAsSource =
-	      Source.fromInputStream(postMethod.getResponseBodyAsStream())
-	    val responseAsString = responseAsSource.getLines.mkString
-	    val response = XML.loadString(responseAsString)
-	    val printer = new PrettyPrinter(1000, 2)
-	    if (status != htmlOk) {
-	    	handleError(req, response)
-	    }
+        postMethod.setRequestEntity(new StringRequestEntity(wrap(req), "text/xml", "utf-8"))
+        postMethod.setRequestHeader("SOAPAction", action)    
+        val client = new HttpClient()
+        val status = client.executeMethod(postMethod)
+        val responseAsSource =
+          Source.fromInputStream(postMethod.getResponseBodyAsStream())
+        val responseAsString = responseAsSource.getLines.mkString
+        val response = XML.loadString(responseAsString)
+        val printer = new PrettyPrinter(1000, 2)
+        if (status != htmlOk) {
+            handleError(req, response)
+        }
       response
     } finally {
       if (postMethod != null) postMethod.releaseConnection()
@@ -41,36 +41,36 @@ object TrivialSoapClient extends SoapClient {
   }
   
   private def handleError(req: Elem, res: Elem) = {
-	  val printer = new PrettyPrinter(1000, 2)
-	  val delimiter = "-" * 200
-	  val error = (res \\ "faultstring").text 
-	  throw new SoapException("JIRA error: " + error + "\n" +
-	      "Send:\n" + 
-	      printer.format(req) + "\n" +
-	      delimiter + "\n" +
-	      "Received:\n" +
-	      printer.format(res)
-	  )
+      val printer = new PrettyPrinter(1000, 2)
+      val delimiter = "-" * 200
+      val error = (res \\ "faultstring").text 
+      throw new SoapException("JIRA error: " + error + "\n" +
+          "Send:\n" + 
+          printer.format(req) + "\n" +
+          delimiter + "\n" +
+          "Received:\n" +
+          printer.format(res)
+      )
   }
   
   private def wrap(xml: Elem): String = {
       "<?xml version=\"1.0\" encoding=\"utf-8\"?>" +
-  		<soapenv:Envelope xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" 
-  			xmlns:xsd="http://www.w3.org/2001/XMLSchema"
-  			xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:soap="http://soap.rpc.jira.atlassian.com">
-  		<soapenv:Header/>
-  			<soapenv:Body>
-  			{xml}
-  			</soapenv:Body>
-  		</soapenv:Envelope>.toString
+        <soapenv:Envelope xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" 
+            xmlns:xsd="http://www.w3.org/2001/XMLSchema"
+            xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:soap="http://soap.rpc.jira.atlassian.com">
+        <soapenv:Header/>
+            <soapenv:Body>
+            {xml}
+            </soapenv:Body>
+        </soapenv:Envelope>.toString
   }  
 }
 
 object JiraSoapMessages {
-	private[jira] val jiraEndpoint = "/rpc/soap/jirasoapservice-v2"
-	  
-	private case class LoginSettings(val token: String, val project: String, 
-	    val issueTypeId: String, val subissueTypeId: String)
+    private[jira] val jiraEndpoint = "/rpc/soap/jirasoapservice-v2"
+      
+    private case class LoginSettings(val token: String, val project: String, 
+        val issueTypeId: String, val subissueTypeId: String)
 }
 
 class JiraSoapMessages(host: String, user: String, password: String, client: SoapClient = TrivialSoapClient) {
@@ -91,9 +91,9 @@ class JiraSoapMessages(host: String, user: String, password: String, client: Soa
   private def login(): String = {
     val req = 
           <soap:login soapenv:encodingStyle="http://schemas.xmlsoap.org/soap/encoding/">
-      			<in0 xsi:type="xsd:string">{user}</in0>
-    				<in1 xsi:type="xsd:string">{password}</in1>
-    			</soap:login>
+                <in0 xsi:type="xsd:string">{user}</in0>
+                    <in1 xsi:type="xsd:string">{password}</in1>
+                </soap:login>
     val res = client.sendMessage(url, "login", req)
     val tokenRes = (res \\ "loginReturn").headOption
     tokenRes.get.text
@@ -101,63 +101,63 @@ class JiraSoapMessages(host: String, user: String, password: String, client: Soa
   
   def logout() {
     if (loginSettings.isDefined) {
-	    val req = 
-	          <soap:logout soapenv:encodingStyle="http://schemas.xmlsoap.org/soap/encoding/">
-      			<in0 xsi:type="xsd:string">{loginSettings.get.token}</in0>
-    			</soap:logout>  
-	    client.sendMessage(url, "logout", req)    
-	    loginSettings = None
+        val req = 
+              <soap:logout soapenv:encodingStyle="http://schemas.xmlsoap.org/soap/encoding/">
+                <in0 xsi:type="xsd:string">{loginSettings.get.token}</in0>
+                </soap:logout>  
+        client.sendMessage(url, "logout", req)    
+        loginSettings = None
     }
   }
   
   def createIssue(summary: String, description:String): String = {
     val req = 
         <soap:createIssue soapenv:encodingStyle="http://schemas.xmlsoap.org/soap/encoding/">
-      		<in0 xsi:type="xsd:string">{loginSettings.get.token}</in0>
-    			<in1>
-    				<project>{loginSettings.get.project}</project>
-    				<summary>{summary}</summary>
-				<description>{description}</description>
-    				<type>{loginSettings.get.issueTypeId}</type>
-    			</in1>
-    		</soap:createIssue>
+            <in0 xsi:type="xsd:string">{loginSettings.get.token}</in0>
+                <in1>
+                    <project>{loginSettings.get.project}</project>
+                    <summary>{summary}</summary>
+                <description>{description}</description>
+                    <type>{loginSettings.get.issueTypeId}</type>
+                </in1>
+            </soap:createIssue>
     val res = client.sendMessage(url, "createIssue", req)
-    (res \\ "key").head.text  	
+    (res \\ "key").head.text    
   }
   
   def createSubissue(parentId: String, summary:String, description:String): String = {
     val req = 
         <soap:createIssueWithParent soapenv:encodingStyle="http://schemas.xmlsoap.org/soap/encoding/">
-      			<in0 xsi:type="xsd:string">{loginSettings.get.token}</in0>
-    				<in1>
-    					<project>{loginSettings.get.project}</project>
-    					<summary>{summary}</summary>
-    					<description>{description}</description>
-    					<type>{loginSettings.get.subissueTypeId}</type>
-    				</in1>
-    				<in2>{parentId}</in2>
-    			</soap:createIssueWithParent>
+            <in0 xsi:type="xsd:string">{loginSettings.get.token}</in0>
+                <in1>
+                    <project>{loginSettings.get.project}</project>
+                    <summary>{summary}</summary>
+                    <description>{description}</description>
+                    <type>{loginSettings.get.subissueTypeId}</type>
+                </in1>
+                <in2>{parentId}</in2>
+            </soap:createIssueWithParent>
     val res = client.sendMessage(url, "createIssueWithParent", req)
-    (res \\ "key").head.text  	
+    (res \\ "key").head.text    
   }  
   
   private def getProjectByKey(token: String, project: String) =  {
-  	val req = 
+    val req = 
       <soap:getProjectByKey soapenv:encodingStyle="http://schemas.xmlsoap.org/soap/encoding/">
-      	<in0 xsi:type="xsd:string">{token}</in0>
-  			<in1 xsi:type="xsd:string">{project}</in1>
-    	</soap:getProjectByKey>
+        <in0 xsi:type="xsd:string">{token}</in0>
+            <in1 xsi:type="xsd:string">{project}</in1>
+        </soap:getProjectByKey>
     val res = client.sendMessage(url, "getProjectByKey", req)
     println(new PrettyPrinter(1000,2).format(res))
-    (res \\ "id").head.text  	
+    (res \\ "id").head.text     
   }
 
   private def getIssueTypeId(token: String, project: String, issuetypeName: String) = {
     val req = 
       <soap:getIssueTypesForProject soapenv:encodingStyle="http://schemas.xmlsoap.org/soap/encoding/">
-      	<in0 xsi:type="xsd:string">{token}</in0>
-    	  <in1 xsi:type="xsd:string">{project}</in1>
-    	</soap:getIssueTypesForProject>
+        <in0 xsi:type="xsd:string">{token}</in0>
+          <in1 xsi:type="xsd:string">{project}</in1>
+        </soap:getIssueTypesForProject>
     val res = client.sendMessage(url, "getIssueTypesForProject", req)
     val list = (res \\ "multiRef").map { el => 
       ((el \ "name").text, (el \ "id").text) 
@@ -169,9 +169,9 @@ class JiraSoapMessages(host: String, user: String, password: String, client: Soa
   private def getSubissueTypeId(token: String, project: String, issuetypeName: String) = {
     val req = 
       <soap:getSubTaskIssueTypesForProject soapenv:encodingStyle="http://schemas.xmlsoap.org/soap/encoding/">
-      	<in0 xsi:type="xsd:string">{token}</in0>
-    	  <in1 xsi:type="xsd:string">{project}</in1>
-    	</soap:getSubTaskIssueTypesForProject>
+        <in0 xsi:type="xsd:string">{token}</in0>
+          <in1 xsi:type="xsd:string">{project}</in1>
+        </soap:getSubTaskIssueTypesForProject>
     val res = client.sendMessage(url, "getSubTaskIssueTypesForProject", req)
     val list = (res \\ "multiRef").map { el => 
       ((el \ "name").text, (el \ "id").text) 
